b42a0265724c358edacf94ef77065bf83f907d1c
vasil-aselectric <vasil.aselectric@gmail.com>
2026-03-17 14:52:59 +0100

    Pedir frecuencia principal

--- test_read_regs.py
+++ test_read_regs.py
@@ -2,6 +2,6 @@ from app.conexion_hv100 import make_instrument, read_reg
 
 inst = make_instrument()
 
-for addr in [27392, 27393, 27394, 27395]:
+for addr in [7425]:
     value = read_reg(inst, addr)
     print(addr, value)
