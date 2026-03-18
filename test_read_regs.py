241b784ce322da55e71b3317ee999f077d249554
vasil-aselectric <vasil.aselectric@gmail.com>
2026-03-18 13:07:40 +0100

    Cambiar registro

--- test_read_regs.py
+++ test_read_regs.py
@@ -7,7 +7,7 @@ registros = {7425: "d-00.00 = Frecuencia de referencia pantalla",
              27393: "11.01 = Numero de esclavo",
              27394: "11.02 = Bbaud (0: 2400BPS, 1: 4800BPS, 2: 9600BPS, 3: 19200BPS, 4: 38400BPS,5: 115200BPS)",
              27395: "11.03 = Date format (0: No check( N, 8,1)for RT)",
-             24845: "00.13 = Frecuencia maxima",
+             24589: "00.13 = Frecuencia maxima",
              
              }
 
